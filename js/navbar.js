cec99f0335ded6095bcea991a38c6f6cd4f846c9
Prof. Juan Pablo <profjpnardone@gmail.com>
2024-11-13 11:28:55 -0300

    Header y footer responsive (final) y responsive de index y psicología

---
+++ js/navbar.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+
+function abrirMenu(){
+    nav.classList.add("visible");
+}
+function cerrarMenu(){
+    nav.classList.remove("visible");
+}
+
+abrir.addEventListener("click", abrirMenu)
+cerrar.addEventListener("click", cerrarMenu)
